557b4c6bed8bf0c2d1275ea06e548ac1e17f5625
Cihan <cihangonen35@gmail.com>
2021-11-04 20:28:07 +0300

    auth is handled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 import './App.css'
 
@@ -9,21 +9,40 @@ import Create from './pages/create/Create';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const {user, authIsReady} = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <div className="container">
-          <Routes>
-            <Route exact path='/' element={<Dashboard/>} />
-            <Route path='/create' element={<Create/>}/>
-            <Route path='/signup' element={<Signup/>} />
-            <Route path='/login' element={<Login/>} />
-            <Route path='/projects/:id' element={<Project/>} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route exact path='/' element=
+                { user ? <Dashboard /> : <Navigate replace to='login'/>}
+              />
+              <Route path='/create' element=
+                { user ? <Create /> : <Navigate replace to='/login'/>}
+              />
+              <Route path='/projects/:id' element=
+                { user ? <Project /> : <Navigate replace to='/login'/>}
+              />
+              <Route path='/signup' element=
+                { !user ? <Signup /> : <Navigate replace to='/'/>}
+              />
+              <Route path='/login' element=
+                { !user ? <Login /> : <Navigate replace to='/'/>}
+              />
+          
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
